7734df69ec31afd87bbfe32a31934d792210839b
TanMusong <tanmusong@gmail.com>
2023-04-21 19:09:13 +0800

    feat 修改构建配置

---
+++ assets/script/index/Index.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node,director, Director } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Index')
+export class Index extends Component {
+
+    protected onLoad() {
+        // director.on(Director.)
+    }
+}
+
