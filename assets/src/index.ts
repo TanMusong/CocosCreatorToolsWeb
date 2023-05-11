1ecab7c3fd4e554369cda4c953f7a3fb91ed3bc3
TanMusong <tanmusong@gmail.com>
2023-05-11 16:53:57 +0800

    feat 调整设计分辨率

--- assets/src/index.ts
+++ assets/src/index.ts
@@ -1,11 +1,24 @@
-import { _decorator, Component, Node,director, Director } from 'cc';
+import { _decorator, Component, Node, director, Director, view, View, Canvas, screen, ResolutionPolicy } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Index')
 export class Index extends Component {
 
+    private resizeScene: boolean = true;
+
     protected onLoad() {
-        // director.on(Director.)
+        view.resizeWithBrowserSize(true);
+        view.setResizeCallback(() => { this.resizeScene = true });
     }
+
+    protected lateUpdate(dt: number): void {
+        if (!this.resizeScene) return;
+        this.resizeScene = false;
+        const designResolution = view.getDesignResolutionSize()
+        const designScale = designResolution.width / designResolution.height;
+        const screenScale = screen.windowSize.width / screen.windowSize.height;
+        view.setResolutionPolicy(screenScale < designScale ? ResolutionPolicy.FIXED_WIDTH : ResolutionPolicy.FIXED_HEIGHT);
+    }
+
 }
 
