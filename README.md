86d25f0d6c776f29ddcb9e9d36cc3ed3909fae5c
TanMusong <tanmusong@gmail.com>
2023-06-29 17:09:04 +0800

    feat bezier test

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# cocoscreator-utils.tanmusong.net
+
+CocosCreator工具集
+
+## 功能列表
+
+- Bezier动画示例
+- ...
+
+
+
+## 使用
+访问[cocoscreator-utils.tanmusong.net](cocoscreator-utils.tanmusong.net)。
--- assets/bezier/src/bezier.ts
+++ assets/bezier/src/bezier.ts
@@ -1,5 +1,4 @@
-import { color } from 'cc';
-import { Color, Component, EditBox, EventTouch, Graphics, IVec2Like, Node, _decorator } from 'cc';
+import { Component, EditBox, EventTouch, Graphics, IVec2Like, Node, _decorator, Color, Toggle, Tween, tween, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('bezier')
@@ -10,6 +9,12 @@ export class Bezier extends Component {
 
     @property(Color)
     private controlLineColor: Color = null;
+
+    @property(Toggle)
+    private cubicBezierToggle: Toggle = null;
+
+    @property(Node)
+    private controlPoint2UI: Node = null;
 
     @property(Node)
     private startPointNode: Node = null;
@@ -24,20 +29,28 @@ export class Bezier extends Component {
     private endPointNode: Node = null;
 
     @property([EditBox])
-    private startPointEditBoxs: EditBox[] = [];
+    private startPointEditBox: EditBox[] = [];
 
     @property([EditBox])
-    private controlPoint1EditBoxs: EditBox[] = [];
+    private controlPoint1EditBox: EditBox[] = [];
 
     @property([EditBox])
-    private controlPoint2EditBoxs: EditBox[] = [];
+    private controlPoint2EditBox: EditBox[] = [];
 
     @property([EditBox])
-    private endPointEditBoxs: EditBox[] = [];
+    private endPointEditBox: EditBox[] = [];
 
     @property(Graphics)
     private graphicsPanel: Graphics = null;
 
+    @property(Toggle)
+    private animationToggle: Toggle = null;
+
+    @property(Node)
+    private animationNode: Node = null;
+
+    // @property()
+    private animationDuration: number = 2;
 
     private isCubicBezier: boolean = true;
     private isNeedUpdate: boolean = false;
@@ -50,29 +63,45 @@ export class Bezier extends Component {
 
     protected onLoad(): void {
 
-        this.startPoint = { x: 0, y: 0 };
-        this.endPoint = { x: 0, y: 0 };
-        this.controlPoint1 = { x: 0, y: 0 };
-        this.controlPoint2 = { x: 0, y: 0 };
+        this.isCubicBezier = this.cubicBezierToggle.isChecked;
+
+
+        this.startPoint = { x: -300, y: 0 };
+        this.endPoint = { x: 300, y: 0 };
+        this.controlPoint1 = this.isCubicBezier ? { x: -100, y: 200 } : { x: 0, y: 200 };
+        this.controlPoint2 = { x: 100, y: -200 };
+
+
+
+        this.cubicBezierToggle.node.on(Toggle.EventType.TOGGLE, this.onCubicBezierToggleClicked, this);
 
         this.setupPointNode(this.startPointNode);
         this.setupPointNode(this.controlPoint1Node);
         this.setupPointNode(this.controlPoint2Node);
         this.setupPointNode(this.endPointNode);
+
+        this.setupPointEditBox(this.startPointEditBox);
+        this.setupPointEditBox(this.controlPoint1EditBox);
+        this.setupPointEditBox(this.controlPoint2EditBox);
+        this.setupPointEditBox(this.endPointEditBox);
+
+        this.onCubicBezierToggleClicked();
+        this.updatePointNodePosition();
+        this.updatePointEditBoxContent();
+        this.updateCubicBezierUI();
+
+
+        this.isNeedUpdate = true;
+
+        this.startBezierAnimation();
+
     }
 
-    private setupPointNode(node: Node): void {
-        node.on(Node.EventType.TOUCH_START, () => node.on(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
-        node.on(Node.EventType.TOUCH_CANCEL, () => node.off(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
-        node.on(Node.EventType.TOUCH_END, () => node.off(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
-    }
-
-    protected update(dt: number): void {
+    protected update(_dt: number): void {
         if (!this.isNeedUpdate) return;
         this.isNeedUpdate = false;
         this.graphicsPanel.clear();
         if (this.isCubicBezier) {
-
             this.graphicsPanel.lineWidth = 2;
             this.graphicsPanel.strokeColor = this.controlLineColor;
 
@@ -88,8 +117,6 @@ export class Bezier extends Component {
             this.graphicsPanel.moveTo(this.startPoint.x, this.startPoint.y);
             this.graphicsPanel.bezierCurveTo(this.controlPoint1.x, this.controlPoint1.y, this.controlPoint2.x, this.controlPoint2.y, this.endPoint.x, this.endPoint.y);
             this.graphicsPanel.stroke();
-
-
         } else {
             this.graphicsPanel.lineWidth = 2;
             this.graphicsPanel.strokeColor = this.controlLineColor;
@@ -108,11 +135,14 @@ export class Bezier extends Component {
         }
     }
 
-    private onPointTouchMove(event: EventTouch): void {
-        const node: Node = event.target;
-        const delta = event.getDelta();
-        node.setPosition(node.position.x + delta.x, node.position.y + delta.y);
-        this.onPointMove();
+    private setupPointNode(node: Node): void {
+        node.on(Node.EventType.TOUCH_START, () => node.on(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
+        node.on(Node.EventType.TOUCH_CANCEL, () => node.off(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
+        node.on(Node.EventType.TOUCH_END, () => node.off(Node.EventType.TOUCH_MOVE, this.onPointTouchMove, this));
+    }
+
+    private setupPointEditBox(editBox: EditBox[]): void {
+        editBox.forEach(value => value.node.on(EditBox.EventType.TEXT_CHANGED, this.onPointInput, this));
     }
 
     private readEditBoxNumber(editBox: EditBox): number {
@@ -121,49 +151,118 @@ export class Bezier extends Component {
         return value;
     }
 
-    protected onPointMove(): void {
+    private onCubicBezierToggleClicked(): void {
+        this.isCubicBezier = this.cubicBezierToggle.isChecked;
+        this.updateCubicBezierUI();
+        this.isNeedUpdate = true;
+    }
+
+    private onPointTouchMove(event: EventTouch): void {
+        const node: Node = event.target;
+        const delta = event.getDelta();
+        node.setPosition(node.position.x + delta.x, node.position.y + delta.y);
+        this.onPointMove();
+    }
+
+    private onPointMove(): void {
         this.startPoint.x = this.startPointNode.position.x;
         this.startPoint.y = this.startPointNode.position.y;
-        this.startPointEditBoxs[0].string = this.startPoint.x.toFixed(1);
-        this.startPointEditBoxs[1].string = this.startPoint.y.toFixed(1);
 
         this.endPoint.x = this.endPointNode.position.x;
         this.endPoint.y = this.endPointNode.position.y;
-        this.endPointEditBoxs[0].string = this.endPoint.x.toFixed(1);
-        this.endPointEditBoxs[1].string = this.endPoint.y.toFixed(1);
 
         this.controlPoint1.x = this.controlPoint1Node.position.x;
         this.controlPoint1.y = this.controlPoint1Node.position.y;
-        this.controlPoint1EditBoxs[0].string = this.controlPoint1.x.toFixed(1);
-        this.controlPoint1EditBoxs[1].string = this.controlPoint1.y.toFixed(1);
 
         this.controlPoint2.x = this.controlPoint2Node.position.x;
         this.controlPoint2.y = this.controlPoint2Node.position.y;
-        this.controlPoint2EditBoxs[0].string = this.controlPoint2.x.toFixed(1);
-        this.controlPoint2EditBoxs[1].string = this.controlPoint2.y.toFixed(1);
+
+        this.updatePointEditBoxContent();
 
         this.isNeedUpdate = true;
     }
 
-    protected onPointInput(): void {
+    private onPointInput(): void {
 
-        this.startPoint.x = this.readEditBoxNumber(this.startPointEditBoxs[0]);
-        this.startPoint.y = this.readEditBoxNumber(this.startPointEditBoxs[1]);
-        this.startPointNode.setPosition(this.startPoint.x, this.startPoint.y);
+        this.startPoint.x = this.readEditBoxNumber(this.startPointEditBox[0]);
+        this.startPoint.y = this.readEditBoxNumber(this.startPointEditBox[1]);
 
-        this.endPoint.x = this.readEditBoxNumber(this.endPointEditBoxs[0]);
-        this.endPoint.y = this.readEditBoxNumber(this.endPointEditBoxs[1]);
-        this.endPointNode.setPosition(this.endPoint.x, this.endPoint.y);
+        this.endPoint.x = this.readEditBoxNumber(this.endPointEditBox[0]);
+        this.endPoint.y = this.readEditBoxNumber(this.endPointEditBox[1]);
 
-        this.controlPoint1.x = this.readEditBoxNumber(this.controlPoint1EditBoxs[0]);
-        this.controlPoint1.y = this.readEditBoxNumber(this.controlPoint1EditBoxs[1]);
-        this.controlPoint1Node.setPosition(this.controlPoint1.x, this.controlPoint1.y);
+        this.controlPoint1.x = this.readEditBoxNumber(this.controlPoint1EditBox[0]);
+        this.controlPoint1.y = this.readEditBoxNumber(this.controlPoint1EditBox[1]);
 
-        this.controlPoint2.x = this.readEditBoxNumber(this.controlPoint2EditBoxs[0]);
-        this.controlPoint2.y = this.readEditBoxNumber(this.controlPoint2EditBoxs[1]);
-        this.controlPoint2Node.setPosition(this.controlPoint2.x, this.controlPoint2.y);
+        this.controlPoint2.x = this.readEditBoxNumber(this.controlPoint2EditBox[0]);
+        this.controlPoint2.y = this.readEditBoxNumber(this.controlPoint2EditBox[1]);
+
+        this.updatePointNodePosition();
 
         this.isNeedUpdate = true;
+    }
+
+    private updateCubicBezierUI(): void {
+        this.controlPoint2Node.active = this.isCubicBezier;
+        this.controlPoint2UI.active = this.isCubicBezier;
+    }
+
+    private updatePointNodePosition(): void {
+        this.startPointNode.setPosition(this.startPoint.x, this.startPoint.y);
+        this.endPointNode.setPosition(this.endPoint.x, this.endPoint.y);
+        this.controlPoint1Node.setPosition(this.controlPoint1.x, this.controlPoint1.y);
+        this.controlPoint2Node.setPosition(this.controlPoint2.x, this.controlPoint2.y);
+    }
+
+    private updatePointEditBoxContent(): void {
+        this.startPointEditBox[0].string = this.startPoint.x.toFixed(1);
+        this.startPointEditBox[1].string = this.startPoint.y.toFixed(1);
+
+        this.endPointEditBox[0].string = this.endPoint.x.toFixed(1);
+        this.endPointEditBox[1].string = this.endPoint.y.toFixed(1);
+
+        this.controlPoint1EditBox[0].string = this.controlPoint1.x.toFixed(1);
+        this.controlPoint1EditBox[1].string = this.controlPoint1.y.toFixed(1);
+
+        this.controlPoint2EditBox[0].string = this.controlPoint2.x.toFixed(1);
+        this.controlPoint2EditBox[1].string = this.controlPoint2.y.toFixed(1);
+    }
+
+    private startBezierAnimation(): void {
+
+        // https://github.com/cocos/cocos-awesome-tech-solutions/blob/3.7.x-release/demo/Creator3.7.3_GameTimeScale/assets/scripts/ModelTween.ts#L20
+
+        const bezierPoint = new Vec3(this.startPoint.x, this.startPoint.y, 0);
+        const bezierTween = new Tween(bezierPoint);
+        const progressX = (_start: number, _end: number, current: number, ratio: number) => {
+            current = this.isCubicBezier ?
+                this.bezier(this.startPoint.x, this.controlPoint1.x, this.controlPoint2.x, this.endPoint.x, ratio) :
+                this.quadratic(this.startPoint.x, this.controlPoint1.x, this.endPoint.x, ratio)
+            return current;
+        };
+
+        const progressY = (_start: number, _end: number, current: number, ratio: number) => {
+            current = this.isCubicBezier ?
+                this.bezier(this.startPoint.y, this.controlPoint1.y, this.controlPoint2.y, this.endPoint.y, ratio) :
+                this.quadratic(this.startPoint.y, this.controlPoint1.y, this.endPoint.y, ratio)
+            return current;
+        };
+
+
+        const easing = "smooth"
+        bezierTween.parallel(
+            tween().to(this.animationDuration, { x: this.endPoint.x }, { progress: progressX, easing, onUpdate: () => this.animationNode.setPosition(bezierPoint) }),
+            tween().to(this.animationDuration, { y: this.endPoint.y }, { progress: progressY, easing, onUpdate: () => this.animationNode.setPosition(bezierPoint) }),
+        ).repeatForever().start();
+    }
+
+    private bezier(start: number, control1: number, control2: number, end: number, ratio: number): number {
+        const t1 = 1 - ratio;
+        return t1 * (t1 * (start + (control1 * 3 - start) * ratio) + control2 * 3 * ratio * ratio) + end * ratio * ratio * ratio;
+    }
+
+    private quadratic(start: number, control: number, end: number, ratio: number): number {
+        const t1 = 1 - ratio;
+        return t1 * t1 * start + 2 * ratio * t1 * control + ratio * ratio * end;
     }
 }
 
